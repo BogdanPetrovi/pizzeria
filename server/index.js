8a7538d0ec6e4b1b578967fe85ebcae3604ca6f4
Bogdan <bogdan.pettrovic@gmail.com>
2025-01-06 12:39:05 +0100

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}!`);
+})
